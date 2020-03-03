a50f76559696f6282a4376602f6a29210e829827
Maitrik Patel <59853047+maitrikpatel2025@users.noreply.github.com>
2020-03-02 20:24:45 -0500

    Update ContactForm.js

--- Form and Control/app/src/Components/ContactForm/ContactForm.js
+++ Form and Control/app/src/Components/ContactForm/ContactForm.js
@@ -1,32 +1,77 @@
 import React,{useState} from 'react'; 
 
 const ContactForm = (props) => {
+
+    //states
     const[name,setName] = useState('');
-    
- const {title}=props;
+    const[email,setEmail] = useState('');
+    const[message,setMessage] = useState('');
+
+    //props
+    const {title}=props;
+
+    const handleSubmit =()=> {
+        //console.log(name);
+        //console.log(email);
+        //console.log(message);
+        const data ={
+            name, // ES6 for name: name,
+            email,
+            message,
+        }
+        console.log(data);
+    }
  return(
      <div className="ContactForm">
          <h3>{title}</h3>
-         <div className="row">
-             <div className="col-6">
-             <div className="from-group">
-             <label htmlFor="name">Name</label>
-             <input type="text" id="name" className="form-control"/>
-             </div>
-             <div className="from-group">
-             <label htmlFor="email">Email</label>
-             <input type="email" id="email" className="form-control"/>
-             </div>
-             <div className="from-group">
-             <label htmlFor="message">Message</label>
-             <textarea rows="5" id="message" className="form-control"> </textarea>
-             </div>
-             <div className="from-group">
-            <button type="button" className="btn btn-success">Submit</button>
-             </div>
+            <div className="row">
+                <div className="col-6">
+                    <div className="from-group">
+                        <label htmlFor="name">Name</label>
+                            <input 
+                                type="text"
+                                id="name" 
+                                className="form-control"
+                                placeholder="Name"
+                                value={name}
+                                onChange={(event) => {
+                                    setName(event.target.value);
+                                    }}/>
+                    </div>
+                    <div className="from-group">
+                        <label htmlFor="email">Email</label>
+                            <input 
+                                type="email" 
+                                id="email" 
+                                className="form-control"
+                                value={email}
+                                onChange={(event) => {
+                                    setEmail(event.target.value);
+                                    }}/>
+                    </div>
+                    <div className="from-group">
+                        <label htmlFor="message">Message</label>
+                            <textarea 
+                                rows="5" 
+                                id="message" 
+                                className="form-control"
+                                value={message}
+                                onChange={(event) => {
+                                    setMessage(event.target.value);
+                                    }}>
+                            </textarea>
+                    </div>
+                    <div className="from-group">
+                        <button 
+                            type="button" 
+                            className="btn btn-success"
+                            onClick={handleSubmit}
+                         >Submit
+                        </button>
+                    </div>
+                </div>
              </div>
          </div>
-     </div>
  )
 }
 export default ContactForm;

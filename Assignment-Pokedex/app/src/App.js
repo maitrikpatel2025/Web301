dcad7c076ffa5218857cc25682c0841e6744d9f0
Maitrik Patel <59853047+maitrikpatel2025@users.noreply.github.com>
2020-04-02 15:14:43 -0400

    web 301

---
+++ Assignment-Pokedex/app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import NavBar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
